7a63573569295aabe186857e5f78664c4dc254e2
vishalkhulape123 <142664838+vishalkhulape123@users.noreply.github.com>
2023-12-17 23:27:22 +0530

    Scholar Lab Day 2

--- ScholatHat_Labs/Labs/Labs/CalculateSquareArea.cs
+++ ScholatHat_Labs/Labs/Labs/CalculateSquareArea.cs
@@ -31,7 +31,11 @@ namespace Labs
                 return _option;
             }
             set
-            { 
+            {
+                if ((value != 1) && (value != 2))
+                {
+                    throw new Exception("Invalid Option");
+                }
                 _option = value;
             }
         }
@@ -79,6 +83,46 @@ namespace Labs
         {
             this._perimeter = 4 * this.length;
             return this.perimeter;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Square square = new Square();
+            //
+            //calculate area of square
+            //Geometrycally size of square are equal
+            //So we consider size of square is legnth
+            while (true)
+            {
+                Console.WriteLine("Enter the length of Square");
+                square.length = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What do you want to calculate?Choose from option ");
+                Console.WriteLine("1: Area of Square");
+                Console.WriteLine("2: Perimeter of Square");
+                square.option = Convert.ToInt32(Console.ReadLine());
+                switch (square.option)
+                {
+                    case 1:
+                        square.CalculateArea();
+                        Console.WriteLine($"The Area of Square is : {square.area}");
+                        break;
+                    case 2:
+                        square.CalculatePerimeter();
+                        Console.WriteLine($"The Perimeter of Square is : {square.perimeter}");
+                        break;
+                    default:
+                        Console.WriteLine("Enter Valid option");
+                        break;
+                }
+                Console.WriteLine("Want to perform again Y/N?");
+                if (char.ToUpper(Console.ReadKey().KeyChar) == 'N')
+                {
+                    break;
+                }
+                Console.ReadLine();
+            }
+            Console.ReadLine();
         }
 
     }
---
+++ ScholatHat_Labs/Labs/Labs/SwapingNumbers.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Labs
+{
+    class SwapingNumbers
+    {
+        public SwapingNumbers() 
+        { 
+        
+        }
+
+        int _number1;
+        int _number2;
+        int _number3;
+
+        public int number1
+        {
+            get
+            { 
+                return _number1;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Invalid value");
+                }
+                _number1 = value;
+            }
+        }
+        public int number2
+        {
+            get
+            {
+                return _number2;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Invalid value");
+                }
+                _number2 = value;
+            }
+        }
+        public int number3
+        {
+            get
+            {
+                return _number3;
+            }
+            set
+            {   if (value < 0)
+                {
+                    throw new Exception("Invalid value");
+                }
+                _number3 = value;
+            }
+        }
+
+        public void SwapingNumberByTemp()
+        { 
+           //number1 = a;  //10
+           //number2 = b;  //20
+            //Console.WriteLine($"Before swaping number1 = {number1} number2 = {number2}");
+            //number3 = c;
+
+            number3 = number1; //10           
+            number1 = number2; //20
+            number2 = number3; //10
+
+           // Console.WriteLine($"After swaping number1 = {number1} number2 = {number2}");
+        }
+
+        public void SwapingNumber()
+        {
+            number1 = number1 + number2;
+            number2 = number1 - number2;
+            number1 = number1 - number2;
+        }
+
+        public void SwapingNum()
+        {
+            number1 = number1 ^ number2;
+            number2 = number1 ^ number2;
+            number1 = number1 ^ number2;
+        }
+
+
+
+        static void Main()
+        {
+            SwapingNumbers swapNum = new SwapingNumbers();
+            Console.WriteLine("Enter Numbers to swap");
+            swapNum.number1 = Convert.ToInt32(Console.ReadLine());
+            swapNum.number2 = Convert.ToInt32(Console.ReadLine());
+            //swapNum.number3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Before swaping number1 = {swapNum.number1} number2 = {swapNum.number2}");
+            swapNum.SwapingNumberByTemp();
+            Console.WriteLine($"After swaping by third variable number1 = {swapNum.number1} number2 = {swapNum.number2}");
+            swapNum.SwapingNumber();
+            Console.WriteLine($"After swaping by arithmetic operation number1 = {swapNum.number1} number2 = {swapNum.number2}");
+            swapNum.SwapingNum();
+            Console.WriteLine($"After swaping by logical operation number1 = {swapNum.number1} number2 = {swapNum.number2}");
+
+            Console.ReadLine();
+        }
+    }
+}
