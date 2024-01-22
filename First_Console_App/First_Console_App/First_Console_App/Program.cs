492bef02e8a489469042af3fce0d95f7f44bb9ca
Vishal Khulape <142664838+vishalkhulape123@users.noreply.github.com>
2024-01-22 23:02:27 +0530

    Delete First_Console_App directory

--- First_Console_App/First_Console_App/First_Console_App/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Hello, World!");
-        Console.ReadKey();
-    }
-}
